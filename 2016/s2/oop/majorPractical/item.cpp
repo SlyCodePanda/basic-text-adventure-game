1685d5acd7c6b06483248183bb46e627a0d368df
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2016-10-26 06:37:23 +0000

    files

--- 2016/s2/oop/majorPractical/item.cpp
+++ 2016/s2/oop/majorPractical/item.cpp
@@ -1,42 +1,47 @@
-#include "item.h"
-#include "player.h"
 #include <string>
 #include <iostream>
 #include <stdlib.h>
 
+#include "item.h"
+#include "player.h"
+
 using namespace std;
+
+// Forward decalration of the Player class.
+
 class Player;
 
-Potion::Potion(string potName, int healEff)
+// Basic constructor of a potion item, taking in the item's name and item effect.
+
+Potion::Potion( string potName, int healEff )
 {
-	name = potName;
-	effect = healEff;
+  name = potName;
+  effect = healEff;
 }
 
-void Potion::apply(Player *player1)
+void Potion::apply( Player *player1 )
 {
-	player1->health += effect;
-	cout << "You drink " << name << "and restore " << effect << " points of health" << endl;
-	cin;
-
-	
+  player1->health += effect;
+  cout << "You drink " << name << "and restore " << effect << " points of health" << endl;
+  cin;	
 }
 
 Potion::~Potion()
 {
 }
 
-Weapon::Weapon(string weapName, int weapAtk)
+Weapon::Weapon( string weapName, int weapAtk )
 {
-	name = weapName;
-	effect = weapAtk;
+  name = weapName;
+  effect = weapAtk;
 }
 
-void Weapon::apply(Player *player1)
+// Allows the user to equip an item to their player, with the effects from said item taking immediate effect over the players stats.
+void Weapon::apply( Player *player1 )
 {
-	player1->weaponDamage = effect;
-	cout << "You have equipped " << name << endl;
-	cin;
+  player1->weaponDamage = effect;
+  cout << "You have equipped " << name << endl;
+  cin;
 }
 
 Weapon::~Weapon()
--- 2016/s2/oop/majorPractical/item.h
+++ 2016/s2/oop/majorPractical/item.h
@@ -1,35 +1,53 @@
 #ifndef ITEM_H
 #define ITEM_H
-#include "player.h"
+
 #include <string>
 #include <iostream>
 
+#include "player.h"
+
 using namespace std;
+
+// Forward declaration of the Player class.
+
 class Player;
+
+// Applies the effect of the selected object to the player.
+// Will replenish the players by the value of effect
 
 class Item
 {
-	public:
-	virtual void apply(Player *player1) = 0;	//Applies the effect of the selected object to the player
-	
-	string name;
-	int effect;
+ public:
+  virtual void apply( Player *player1 ) = 0;
+  
+  string name;
+  int effect;
 };
+
+// Sub-class of item.
+// Potion will effect player's health points.
 
 class Potion: public Item
 {
-	public:
-	Potion(string potName, int healEff);
-	void apply(Player *player1);	//Will replenish the players by the value of effect
-	~Potion();
+ public:
+  Potion( string potName, int healEff );
+  
+  void apply( Player *player1 );
+  
+  ~Potion();
 };
+
+// Sub-class of item.
+// Will set the value of weapon damage in the player class to the value effect.
 
 class Weapon: public Item
 {
-	public:
-	Weapon(string weapName, int weapAtk);
-	void apply(Player *player1); //Will set the value of weapon damage in the player class to the value effect
-	~Weapon();
+ public:
+  Weapon( string weapName, int weapAtk );
+  
+  void apply( Player *player1 );
+  
+  ~Weapon();
 
 };
 
